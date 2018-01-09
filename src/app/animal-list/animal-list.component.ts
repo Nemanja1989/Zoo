d9654f21746eb82af5644a2dd714276b3a9fa94e
Nemanja Bojanic <nemanja.bojanic@pool4tool.com>
2018-01-09 21:12:52 +0100

    master task 6 done

--- src/app/animal-list/animal-list.component.ts
+++ src/app/animal-list/animal-list.component.ts
@@ -1,33 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Animal} from '../animal';
 
 @Component({
-  selector: 'app-animal-list',
-  templateUrl: './animal-list.component.html',
-  styleUrls: ['./animal-list.component.css']
+    selector: 'app-animal-list',
+    templateUrl: './animal-list.component.html',
+    styleUrls: ['./animal-list.component.css']
 })
 export class AnimalListComponent implements OnInit {
 
     animals: Array<Object>;
+    animal;
+    species;
+    name;
+    date_of_birth;
 
     constructor() {
 
         this.animals = [
-            new Animal('Mrk','Dalibor','06-06-1989'),
-            new Animal('Horse','Sima','06-06-2006'),
-            new Animal('Dog','Joca',''),
-            new Animal('Cat','Mile','01-03-1989')
+            new Animal('Mrk', 'Dalibor', '06-06-1989'),
+            new Animal('Horse', 'Sima', '06-06-2006'),
+            new Animal('Dog', 'Joca', ''),
+            new Animal('Cat', 'Mile', '01-03-1989')
         ];
-        console.log(this.animals);
 
-  }
+    }
 
     removeAnimal(animal) {
         const index = this.animals.indexOf(animal);
-        this.animals.splice(index, 1);
     }
 
-  ngOnInit() {
-  }
+    moveOnTop(animal) {
+        const index = this.animals.indexOf(animal);
+        this.animals.splice(index, 1);
+        this.animals.unshift(animal);
+    }
+
+    addAnimal() {
+        this.animals.push(new Animal(this.name, this.species, this.date_of_birth));
+    }
+
+    ngOnInit() {
+    }
 
 }
