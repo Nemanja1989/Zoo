8d7743a583491b22610a54c73b0144c4f0d7265c
Nemanja Bojanic <nemanja.bojanic@pool4tool.com>
2018-01-10 08:56:13 +0100

    master task 8 done

--- src/app/animal-list/animal-list.component.ts
+++ src/app/animal-list/animal-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Animal} from '../animal';
+import {Sector} from '../sector';
 
 @Component({
     selector: 'app-animal-list',
@@ -9,18 +10,25 @@ import {Animal} from '../animal';
 export class AnimalListComponent implements OnInit {
 
     animals: Array<Object>;
-    animal;
     species;
     name;
     date_of_birth;
+    sector;
+    sectors;
 
     constructor() {
 
+        this.sectors = [
+            new Sector('sector 1', 'surface 1'),
+            new Sector('sector 2', 'surface 2'),
+            new Sector('sector 3', 'surface 3')
+        ];
+
         this.animals = [
-            new Animal('Mrk', 'Dalibor', '06-06-1989'),
-            new Animal('Horse', 'Sima', '06-06-2006'),
-            new Animal('Dog', 'Joca', ''),
-            new Animal('Cat', 'Mile', '01-03-1989')
+            new Animal('Mrk', 'Dalibor', '06-06-1989', this.sectors[0]),
+            new Animal('Horse', 'Sima', '06-06-2006', this.sectors[2]),
+            new Animal('Dog', 'Joca', '', this.sectors[0]),
+            new Animal('Cat', 'Mile', '01-03-1989', this.sectors[1])
         ];
 
     }
@@ -36,7 +44,19 @@ export class AnimalListComponent implements OnInit {
     }
 
     addAnimal() {
-        this.animals.push(new Animal(this.name, this.species, this.date_of_birth));
+        this.animals.push(new Animal(this.name, this.species, this.date_of_birth, this.sector));
+    }
+
+    showAnimalsFromSector(sector) {
+        const animals = [];
+
+        for ( let i = 0; i < this.animals.length; i++) {
+            if (this.animals[i]['sector'] === sector) {
+                animals.push(this.animals[i]['name']);
+            }
+        }
+
+        alert(animals.join('\n'));
     }
 
     ngOnInit() {
--- src/app/animal.ts
+++ src/app/animal.ts
@@ -1,13 +1,17 @@
+import { Sector } from './sector';
+
 export class Animal {
 
     species;
     name;
     date_of_birth;
+    sector: Sector;
 
-    constructor(sort: string, name: string, date: string) {
-        this.species     = sort;
-        this.name        = name;
-        this.date_of_birth = date;
+    constructor(sort: string, name: string, date: string, sector: Sector) {
+        this.species        = sort;
+        this.name           = name;
+        this.date_of_birth  = date;
+        this.sector         = sector;
 
     }
 
